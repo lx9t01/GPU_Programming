f915cd00390e80ac4a7272b554b580b6acf5d89b
lx9t01 <alex.xuhaixiang@gmail.com>
2016-04-02 15:19:21 -0700

    convolution 2

--- UNIX/blur_device.cu
+++ UNIX/blur_device.cu
@@ -24,7 +24,7 @@ void cudaBlurKernel(const float *raw_data, const float *blur_v, float *out_data,
     
     while (thread_index < n_frames) {
         if (thread_index < blur_v_size) {
-            for (int i = 0; i < thread_index; ++i) {
+            for (int i = 0; i <= thread_index; ++i) {
                 out_data[thread_index] += raw_data[thread_index - i] * blur_v[i];
             }
         } else {
